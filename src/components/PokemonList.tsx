7ab6492f567ca4695938724e427b65bccbf9b989
Ayoub <ayoub.ouahid@unify-technologies.com>
2023-10-04 10:23:31 +0200

    pokemonlist

---
+++ src/components/PokemonList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+  } from 'react-query'
+
+const PokemonList = () => {
+    const queryClient = useQueryClient()
+
+    return (
+        <div>
+           okay 
+        </div>
+    );
+};  
+
+export default PokemonList;
